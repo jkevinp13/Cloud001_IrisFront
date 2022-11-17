810453dbf2dd039ae6b7430e8782a2f4fdb9473c
Jhan Kevin Peña Borja <jhpena@bancolombia.com.co>
2022-11-17 02:24:46 -0500

    update name for dist

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('prueba2 app is running!');
+    expect(await page.getTitleText()).toEqual('cloud001_irisfront app is running!');
   });
 
   afterEach(async () => {
